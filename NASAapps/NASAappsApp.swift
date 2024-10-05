ff10d82cbaf6e519bcce0c69e5902d367a5cadc3
Aariana <anajshah@gmail.com>
2024-10-05 11:33:30 -0700

    Initial Commit

---
+++ NASAapps/NASAappsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAappsApp.swift
+//  NASAapps
+//
+//  Created by Aariana Shah on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct NASAappsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
